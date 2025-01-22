e0b19425708fb9f48667d7714c99c26c86dc672f
varsha2131 <varshakunchala730@gmail.com>
2025-01-22 20:24:12 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="w-full  bg-gray-800 bg-opacity-30 text-black py-4 text-center">
+        <p>&copy; Copyright © 2025 All rights reserved with Charatered Accoutants Portifolio</p>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Footer;
